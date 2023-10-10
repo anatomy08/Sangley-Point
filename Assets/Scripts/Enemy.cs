b3418465f3ac58da4102f15adcd5ecd97a3459d4
Jeffrey Expectacion <expectacionjeffrey@gmail.com>
2023-10-11 00:02:33 +0800

    finito

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour 
